2f4548e02dad4ef03a99b50c5f614ce047bf0956
AirLin-K70 <2795374549@qq.com>
2024-10-19 16:31:24 +0800

    Add files via upload

--- src/org/example/NumberFeature.java
+++ src/org/example/NumberFeature.java
@@ -4,25 +4,31 @@ import java.util.Scanner;
 
 public class NumberFeature {
     public static void main(String[] args) {
-        int inputNumber; // 输入的数字
-        int result = 0; // 输出结果
-        Scanner in = new Scanner(System.in);
-        inputNumber = in.nextInt();
-        int position = 1;
-        // 你的代码逻辑
-        while (inputNumber > 0) {
-            int digit = inputNumber % 10; // 获取当前最低位的数字
-            inputNumber /= 10; // 去掉当前最低位的数字
+        Scanner scanner = new Scanner(System.in);
+        int nonnegative_number = scanner.nextInt(); // 读取非负整数
+        scanner.close();
 
-            // 判断数字的奇偶性与数位的奇偶性是否相同
-            if ((digit % 2) == (position % 2)) {
-                result += Math.pow(2, position - 1);
+        int a, b = 0, f; // a：输入数每位的存储单元。b：位数。f：存储2为多少次幂（二进制位）
+        int c, d = 0; // c：如果满足题意 奇偶相同为1，否则为零。d：储存结果
+
+        while (nonnegative_number != 0) {
+            a = nonnegative_number % 10; // 获取当前最低位的数字
+            b++; // 位数加1
+            f = b;
+            if (a == 0) {
+                c = 0;
+            } else if (a % 2 == b % 2) { // 判断奇偶性是否相同
+                c = 1;
+                for (; f > 1; f--) {
+                    c = c * 2;
+                }
+            } else {
+                c = 0;
             }
-
-            position++;
+            d += c; // 将c的值加到结果d上
+            nonnegative_number = nonnegative_number / 10; // 去掉当前最低位的数字
         }
 
-
-        System.out.println(result);
+        System.out.println(d); // 输出结果
     }
 }
